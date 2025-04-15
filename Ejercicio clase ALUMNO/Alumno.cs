9906c7aeb3af3f4f616f15226c691b09d44618e3
“Ignacio <nachoatum@gmail.com>
2025-04-15 15:52:32 -0300

    Clase hora y clase alumno

---
+++ Ejercicio clase ALUMNO/Alumno.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: nacho
+ * Date: 15/4/2025
+ * Time: 15:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Ejercicio_clase_ALUMNO
+{
+	/// <summary>
+	/// Description of Alumno.
+	/// </summary>
+	public class Alumno
+	{
+		private string nombre;
+		private string apellido;
+		private int edad;
+		private int legajo;
+		private double nota;
+		
+		public Alumno()
+		{
+		}
+		
+		public Alumno(string nombre, string apellido, int edad, int legajo, double nota){
+			
+			this.nombre = nombre;
+			this.apellido = apellido;
+			this.edad = edad;
+			this.legajo = legajo;
+			this.nota = nota;
+			
+		}
+		
+		public void imprimir(){
+			Console.WriteLine("Nombre y apellido: " + nombre +" "+ apellido +"\n Edad: " + edad + "\n Legajo: " + legajo + "\n Nota: " + nota);
+		}
+	}
+}
---
+++ Ejercicio clase ALUMNO/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: nacho
+ * Date: 15/4/2025
+ * Time: 15:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Ejercicio_clase_ALUMNO
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			Alumno al = new Alumno("Ignacio", "Quiroga", 20, 12345678, 10);
+			
+			al.imprimir();
+			
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}

dbeab3598f80d5715095dbc98f17c0b5d3c08fcc
“Ignacio <nachoatum@gmail.com>
2025-04-15 16:00:36 -0300

    Ejercicio clase hora, este es el realgit add .

---
+++ Ejercicios/HORA.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: nacho
+ * Date: 15/4/2025
+ * Time: 15:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Ejercicios
+{
+	/// <summary>
+	/// Description of HORA.
+	/// </summary>
+	public class HORA
+	{
+		
+		private int hora;
+		private int minuto;
+		private int segundo;
+		
+		public HORA(){
+			/*Constructor vacio*/
+		}
+		
+		public HORA(int hora, int minuto, int segundo){
+			
+			this.hora = hora;
+			this.minuto = minuto;
+			this.segundo = segundo;
+			
+		}
+		
+		public void imprimir(){
+			Console.WriteLine(hora + " HORAS, " + minuto + " MINUTOS Y " + segundo +" SEGUNDOS");
+		}
+		
+		
+	}
+}
---
+++ Ejercicios/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: nacho
+ * Date: 15/4/2025
+ * Time: 15:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+
+namespace Ejercicios
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			/* CLASE HORA TP FIRMINA */
+			
+			HORA h = new HORA(23,30,15);
+			h.imprimir();
+			
+			Console.ReadKey(true);
+		}
+	}
+}
